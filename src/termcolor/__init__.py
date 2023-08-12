cc9341adc9161c6b17b2c1003fb02ca13fd38b43
Can <pekkan.ct@gmail.com>
2023-08-12 13:05:40 -0400

    Update __init__.py

--- src/termcolor/__init__.py
+++ src/termcolor/__init__.py
@@ -1,13 +1,14 @@
 """ANSI color formatting for output in terminal."""
 from __future__ import annotations
 
-from termcolor.termcolor import ATTRIBUTES, COLORS, HIGHLIGHTS, RESET, colored, cprint
+from termcolor.termcolor import ATTRIBUTES, COLORS, HIGHLIGHTS, RESET, colorAdd, colored, cprint
 
 __all__ = [
     "ATTRIBUTES",
     "COLORS",
     "HIGHLIGHTS",
     "RESET",
+    "colorAdd",
     "colored",
     "cprint",
 ]
