6cd214bd66bbfaea539b72c4606809b73cc5b505
MilesWK <121042782+MilesWK@users.noreply.github.com>
2024-11-10 14:36:08 -0500

    Create colored_function_example.py
    📌 - This is an example for the `colored()` function!

---
+++ examples/colored_function_example.py
@@ -0,0 +1,28 @@
+#                                         #
+# EXAMPLE USAGE OF THE COLORED() FUNTCION #
+#                BY MILESWK               #
+#                    :)                   #
+#                                         #
+
+from termcolor import colored # Import the module
+
+# Syntax for "colored()" function: 
+# colored(text, text_color, on_color)
+
+# This is our text normally:
+normal_text = "Hello! Please color me!"
+
+# Print the unformatted text:
+print(normal_text) # >>> Hello! Please color me!
+
+# New variable alert! This one is coloring the text to blue:
+colored_text = colored(normal_text, "blue")
+
+# Let's print the colored version: 
+print(colored_text) # >>> (blue) Hello! Please color me!
+
+# Now let's change the color again:
+colored_text = colored(normal_text, "blue", "on_white")
+
+# Let's print that...
+print(colored_text) # >>> (blue with a white background) Hello! Please color me!
